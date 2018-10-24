bac0afcf8217e0c8e339ee498fb6d30cbede9745
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-24 03:02:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,7 @@ def speak_to_grandma(greeting)
 
 if greeting == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
-end
-if greeting == greeting.upcase && greeting !== "I LOVE YOU GRANDMA!"
+else if greeting == greeting.upcase && greeting !== "I LOVE YOU GRANDMA!"
    "NO, NOT SINCE 1938!"
 else 
    "HUH?! SPEAK UP, SONNY!"
